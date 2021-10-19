905a4537e7be9cba735097e258ce10ea07aa9349
Sandra <sreinert@neuro.mpg.de>
2021-10-19 10:03:42 +0200

    Added authentication function and pwdb dictionary

--- auth.py
+++ auth.py
@@ -3,5 +3,21 @@ def get_credentials():
     password = input('Type password: ')
     return username, password
 
+def authenticate(username, password, pwdb):
+    auth = False
+    if username in pwdb:
+        if password == pwdb[username]:
+            print('Authentication successful!')
+            auth = True
+        else:
+            print('Wrong password!')
+    else:
+        print('User not known!')
+    return auth
+
+pwdb = {'tiziano' : 'abc123',
+        'pamela' : 'querty',
+        'sandra' : 'bla'}
+
 username, password = get_credentials()
-print(username, password)
+authenticate(username, password, pwdb)
